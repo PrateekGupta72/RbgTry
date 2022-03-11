cee37d3e10f6321ddc0c4d63a03b6a8a76acbab3
AniketNandi <aniket.regami@solutions>
2022-03-12 02:09:54 +0530

    some system specific changes and added design files

--- app/src/main/java/com/regami/rbgtrial/MainActivity.java
+++ app/src/main/java/com/regami/rbgtrial/MainActivity.java
@@ -4,16 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.VideoView;
 
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.DefaultTimeBar;
+import com.google.android.exoplayer2.ui.PlayerView;
+
 public class MainActivity extends AppCompatActivity {
+    PlayerView playerView;
+    ImageView playBtn, pauseBtn;
+    DefaultTimeBar seekBar;
+
+    // player to be attached with video_view
+    SimpleExoPlayer player;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        VideoView rbg= findViewById(R.id.RbgVideo);
-        rbg.setVideoURI(Uri.parse("https://www.youtube.com/watch?v=d-iwHP7vMd8&list=PLu0W_9lII9aiL0kysYlfSOUgY5rNlOhUd&index=10"));
-        rbg.start();
+
+        // code for setting up ExoPlayer
+        playerView = findViewById(R.id.video_view);
+        playBtn = findViewById(R.id.play_btn);
+        pauseBtn = findViewById(R.id.pause_btn);
+        seekBar = findViewById(R.id.player_seek_bar);
     }
 }
