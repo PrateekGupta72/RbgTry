656472250dfb295557718ac36fac76127b439684
Prateek Gupta <prateek@regami.solutions>
2022-03-11 17:52:29 +0530

    basic ui

--- app/src/main/java/com/regami/rbgtrial/MainActivity.java
+++ app/src/main/java/com/regami/rbgtrial/MainActivity.java
@@ -2,7 +2,9 @@ package com.regami.rbgtrial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView rbg= findViewById(R.id.RbgVideo);
+        rbg.setVideoURI(Uri.parse("https://www.youtube.com/watch?v=d-iwHP7vMd8&list=PLu0W_9lII9aiL0kysYlfSOUgY5rNlOhUd&index=10"));
+        rbg.start();
     }
 }
