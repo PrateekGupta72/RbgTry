c6831bca31c0fdc176a6a4f087aff790e1b09314
Prateek Gupta <prateek@regami.solutions>
2022-03-12 21:51:58 +0530

    basic ui 2

--- app/src/main/java/com/regami/rbgtrial/MainActivity.java
+++ app/src/main/java/com/regami/rbgtrial/MainActivity.java
@@ -2,22 +2,40 @@ package com.regami.rbgtrial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.VideoView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.DefaultTimeBar;
 import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.ui.StyledPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 
 public class MainActivity extends AppCompatActivity {
-    PlayerView playerView;
+    StyledPlayerView playerView;
     ImageView playBtn, pauseBtn;
     DefaultTimeBar seekBar;
+    Context context;
 
     // player to be attached with video_view
-    SimpleExoPlayer player;
+    ExoPlayer player;
+
+    {
+
+        player = new ExoPlayer.Builder(getApplicationContext()).build();
+    }
 
 
     @Override
@@ -30,5 +48,18 @@ public class MainActivity extends AppCompatActivity {
         playBtn = findViewById(R.id.play_btn);
         pauseBtn = findViewById(R.id.pause_btn);
         seekBar = findViewById(R.id.player_seek_bar);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+         Uri video= Uri.parse("https://www.youtube.com/watch?v=MdQOXoEMLOs&t=178s");
+playerView.setPlayer(player);
+        MediaItem mediaItem=MediaItem.fromUri(video);
+        player.setMediaItem(mediaItem);
+        player.prepare();
+        player.play();
+//        LoadControl loadControl=new DefaultLoadControl();
+//
+//        BandwidthMeter bandwidthMeter= new DefaultBandwidthMeter();
+//        TrackSelector trackSelector=new DefaultTrackSelector();
+
     }
 }
